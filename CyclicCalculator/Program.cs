40d96521098efb42f33f736e98164e0ae301072c
Harry French <harry.french@moonforge.com>
2021-10-11 22:48:53 +0100

    accumulator behaving

--- CyclicCalculator/Program.cs
+++ CyclicCalculator/Program.cs
@@ -6,7 +6,26 @@ namespace CyclicCalculator
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ConsoleKeyInfo key = new ConsoleKeyInfo();
+
+            int total = 0;
+            do{
+                key = Console.ReadKey(true);
+                try{
+                    string str = ""+key.KeyChar;
+                    int val =  Convert.ToInt32(str);
+                    if(total == 0 && val == 0){
+                        continue;
+                    }else{
+                        total *= 10;
+                        total += val;
+                    }
+                    Console.WriteLine("Character: "+str+" "+val+" "+total);
+                }catch(Exception e){
+                    Console.WriteLine("Exception: "+e.Message+" "
+                            +key.Key.ToString());
+                }
+            }while(key.Key != ConsoleKey.Escape);
         }
     }
 }
